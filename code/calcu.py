f4b7c88aa5cd3e9709aa2e2fdcb46795746e6f5c
Sergio71i <s.marinr@unitecnica.net>
2026-09-17 20:16:22 -0500

    cambios en el archivo calcu.py con operaciones

---
+++ code/calcu.py
@@ -0,0 +1,14 @@
+def suma(valor1, valor2):
+    return valor1 + valor2
+
+def resta(valor1, valor2):
+    return valor1 - valor2
+def multi (valor1, valor2):
+    return valor1 * valor2
+
+resultadoSuma = suma(5,2)
+print(resultadoSuma)
+resultadoResta = resta(5,2)
+print(resultadoResta)
+resultadoMultiplicacion = multi(5,2)
+print(resultadoMultiplicacion)
